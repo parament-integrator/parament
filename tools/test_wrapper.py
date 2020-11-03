0fc777db0329ef20e1ba90fdc3286c5120ff1604
kherb <kherb@phys.ethz.ch>
2020-11-03 11:32:26 +0100

    Revert "Revert "parament.c aktualisiert""
    Dies reverted den Commit b96e0946211cd96fdfc49e528f80f4e83401f9f8.

--- tools/test_wrapper.py
+++ tools/test_wrapper.py
@@ -13,20 +13,12 @@ H0 = np.array([[1,0],[0,-1]])
 H1 = np.array([[0,1],[1,0]])
 dt = 1.0
 
-expected_matrix_exponential = scipy.linalg.expm(-1j*dt*H0)
-
 print("Expected: ")
-print(expected_matrix_exponential)
+print(scipy.linalg.expm(-1j*dt*H0))
 
 GPURunner.setHamiltonian(H0,H1)
 
 output_propagator = GPURunner.equiprop(np.zeros(1),dt)
 
-print("Calculated: ")
 print(output_propagator)
-
-diff = expected_matrix_exponential - output_propagator
-diff = diff/(np.finfo(np.float32).eps)
-print("Diff in units of eps")
-print(diff)
 #GPURunner.destroy()
