19ec7f8b7dd1455204d132eee505c42506d031b2
SPINWORK <welterp@phys.ethz.ch>
2021-02-04 14:15:15 +0100

    Feed coverage data into Gitlab

--- tools/runTestPyparament.bat
+++ tools/runTestPyparament.bat
@@ -3,7 +3,7 @@ python -m venv parament_py
 call parament_py\Scripts\activate.bat
 pip install -r ./src/python/requirements.txt  || goto :error
 pip install src\dist\parament-0.1-py3-none-win_amd64.whl || goto :error
-pytest --cov=parament --junitxml=report.xml --pyargs parament || goto :error
+pytest --cov=parament --cov-report xml --cov-report term --junitxml=report_test.xml --pyargs parament || goto :error
 deactivate
 cd ..
 goto :EOF
