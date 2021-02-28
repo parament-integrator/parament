5ed41656998f0b5b92fa9b232b0f6b7380ccb19c
Ubuntu <ubuntu@ip-172-31-73-232>
2021-02-28 16:17:52 +0000

    Update build.sh

---
+++ tools/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SOURCEFILES=("deviceInfo.c" "diagonal_add.cu" "main.cpp" "mathhelper.cpp" "parament.cpp" "printFuncs.cpp" "debugfuncs.cpp" "control_expansion.cu")
+NVCCFLAGS=("-lcublas" "--compiler-options" "-fPIC")
+OUTPUTDIR=build
+
+mkdir -p build
+cd src/cuda
+echo nvcc "${NVCCFLAGS[@]}" -DPARAMENT_LINK -o "../../$OUTPUTDIR/parament" "${SOURCEFILES[@]}" || exit $?
+nvcc "${NVCCFLAGS[@]}" -DPARAMENT_BUILD_DLL -o "../../$OUTPUTDIR/libparament.so" "${SOURCEFILES[@]}" || exit $?
+
+cd ../..
