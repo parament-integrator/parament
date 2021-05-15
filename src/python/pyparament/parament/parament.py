be436ae76e9a1a933dc7a6cf5fad7ab2740db82e
kherb <kherb@phys.ethz.ch>
2021-05-15 10:59:09 +0200

    Licence text

--- src/python/pyparament/parament/parament.py
+++ src/python/pyparament/parament/parament.py
@@ -1,3 +1,18 @@
+# Copyright 2021 Konstantin Herb, Pol Welter. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ##########################################################################
+
+
 import logging
 import ctypes
 import numpy as np
@@ -8,7 +23,7 @@ import parament.paramentlib
 
 logger = logging.getLogger('Parament')
 logging.basicConfig()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.CRITICAL)
 
 # Import qutip if available
 try:
--- tools/build.bat
+++ tools/build.bat
@@ -1,4 +1,18 @@
 @echo off
+REM Copyright 2021 Konstantin Herb, Pol Welter. All Rights Reserved.
+REM 
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM 
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+REM =========================================================================
+
 SETLOCAL
 set sourceFiles=deviceinfo.c diagonal_add.cu main.cpp mathhelper.cpp parament.cpp printFuncs.cpp debugfuncs.cpp control_expansion.cu
 set nvccFlags=-lcublas
@@ -16,5 +30,5 @@ goto :EOF
 
 
 :error
-echo Failed with error #%errorlevel%.
+echo Failed with error REM%errorlevel%.
 exit /b %errorlevel%
